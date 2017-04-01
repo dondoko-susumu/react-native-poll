19cd3927b4bc0cf2676f1758cdd9ff32e06f191a
Ken Kubota <kkbt2003@gmail.com>
2017-04-01 11:58:41 +0900

    init implementation component

--- components/Poll.js
+++ components/Poll.js
@@ -1,17 +1,121 @@
 import React, { Component } from 'react';
 import {
+  Platform,
+  StyleSheet,
   Text,
+  TouchableOpacity,
   View,
+  Dimensions,
 } from 'react-native';
 
+const choices = [
+  {
+    title: 'aaa',
+    votes: [
+      {
+        name: 'user1',
+      },
+    ],
+  },
+  {
+    title: 'bbb',
+    votes: [
+      {
+        name: 'user1',
+      },
+      {
+        name: 'user2',
+      },
+    ],
+  },
+];
+
+const votersCount = 2;
+
 export default class Poll extends Component {
+
+  onPressChoice(choice) {
+    console.log(choice);
+  }
+
+  choiceSelected(choice) {
+    console.log(choice);
+    return true;
+  }
+
   render() {
+    const screenWidth = Dimensions.get('window').width;
+    let graphWidth;
+    if (Platform.OS === 'ios') {
+      graphWidth = screenWidth - (16 * 3) - (8 * 2) - 40 - 4;
+    } else {
+      graphWidth = screenWidth - (16 * 3) - (8 * 2) - 40 - 2;
+    }
+
     return (
       <View>
-        <Text>
-          Hello Poll!
-        </Text>
+        {choices.map(choice => (
+          <View style={styles.rowContainer}>
+            <TouchableOpacity
+              disabled
+              style={styles.avatar}
+              onPress={() => this.onPressChoice(choice)}
+            >
+              <View style={styles.check} backgroundColor={this.choiceSelected(choice) ? '#20B2AA' : '#E4E5EB'} />
+            </TouchableOpacity>
+
+            <View
+              style={styles.container}
+              onLayout={(event) => {
+                const { width } = event.nativeEvent.layout;
+                this.setState({ graphWidth: width });
+              }}
+            >
+              <View style={styles.barContainer}>
+                <View style={[styles.bar, { width: (choice.votes.length / votersCount) * graphWidth }]} />
+                <Text
+                  style={[styles.barItem, { width: graphWidth - 38 }]}
+                  numberOfLines={2}
+                  ellipsizeMode={'tail'}
+                >
+                  {choice.title}
+                </Text>
+                <Text style={[styles.pollsCount]}>{choice.votes.length}</Text>
+              </View>
+            </View>
+          </View>
+          ))}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  rowContainer: {
+    alignItems: 'center',
+    marginTop: 8,
+    flexDirection: 'row',
+  },
+  barContainer: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  bar: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    backgroundColor: '#E4E5EB',
+  },
+  barItem: {
+    fontSize: 14,
+    marginVertical: 6,
+    paddingLeft: 8,
+    backgroundColor: 'transparent',
+  },
+  check: {
+    width: 20,
+    height: 20,
+  },
+});
